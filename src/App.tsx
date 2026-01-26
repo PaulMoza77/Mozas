8661f0fe3dad7b2631b3c78b9bee0d4f4c40291c
Paul Moza <rentalcarsoradea@gmail.com>
2026-01-26 10:12:37 +0200

    moBILE rESPONSIVENESS HOME PAGE DONE

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "./pages/Home";
 import Admin from "./pages/Admin";
+import Expenses from "./pages/Expenses";
 import AdminGate from "./pages/AdminGate";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Public */}
         <Route path="/" element={<Home />} />
 
+        {/* Admin overview */}
         <Route
           path="/admin"
           element={
@@ -18,6 +21,17 @@ function App() {
           }
         />
 
+        {/* Admin – Expenses */}
+        <Route
+          path="/admin/expenses"
+          element={
+            <AdminGate>
+              <Expenses />
+            </AdminGate>
+          }
+        />
+
+        {/* Fallback */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
